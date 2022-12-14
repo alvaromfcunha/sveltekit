1ec8c079e6adb72d2848487069f85e438c213f68
Alvaro <alvaro99.cunha@gmail.com>
2022-12-14 19:35:18 -0300

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<head>
+  <title>Svelte Counter</title>
+</head>
+
+<script>
+  import Counter from './Counter.svelte'
+</script>
+
+<h1>Svelte Counter</h1>
+<Counter />
+<footer>Alvaro Cunha</footer>
+
---
+++ src/routes/Counter.svelte
@@ -0,0 +1,14 @@
+<script>
+  let counter = 0
+
+  function addCounter() {
+    counter += 1
+  }
+  function subCounter() {
+    counter -= 1
+  }
+</script>
+
+<button on:click={addCounter}>+</button>
+{counter}
+<button on:click={subCounter}>-</button>
